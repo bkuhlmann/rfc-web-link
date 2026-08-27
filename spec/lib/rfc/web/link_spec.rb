6113eb618952dcbae5d3c72f962caa685b435a09
Brooke Kuhlmann <brooke@alchemists.io>
2026-08-27 10:59:45 -0600

    Refactored header parser and link specs to use shared examples
    Reduces duplication since we can define the examples once and use across both objects.
    
    Milestone: patch

--- spec/lib/rfc/web/link_spec.rb
+++ spec/lib/rfc/web/link_spec.rb
@@ -3,75 +3,9 @@
 require "spec_helper"
 
 RSpec.describe RFC::Web::Link do
-  subject(:link) { described_class.new "https://test.io" }
+  subject(:parser) { described_class.new "https://test.io" }
 
   describe ".new" do
-    it "answers record for lowercase key" do
-      headers = {"link" => "</articles>; rel=index"}
-
-      expect(link.call(headers)).to eq(
-        RFC::Web::Link::Models::List[
-          links: Set[
-            RFC::Web::Link::Models::Link[
-              uri: "https://test.io/articles",
-              pairs: Set[RFC::Web::Link::Models::Pair[key: :rel, value: "index"]]
-            ]
-          ]
-        ]
-      )
-    end
-
-    it "answers record for uppercase key" do
-      headers = {"Link" => "</articles>; rel=index"}
-
-      expect(link.call(headers)).to eq(
-        RFC::Web::Link::Models::List[
-          links: Set[
-            RFC::Web::Link::Models::Link[
-              uri: "https://test.io/articles",
-              pairs: Set[RFC::Web::Link::Models::Pair[key: :rel, value: "index"]]
-            ]
-          ]
-        ]
-      )
-    end
-
-    it "answers records" do
-      headers = {"Link" => "</articles?page=1>; rel=previous, </articles?page=3>; rel=next"}
-
-      expect(link.call(headers)).to eq(
-        RFC::Web::Link::Models::List[
-          links: Set[
-            RFC::Web::Link::Models::Link[
-              uri: "https://test.io/articles?page=1",
-              pairs: Set[RFC::Web::Link::Models::Pair[key: :rel, value: "previous"]]
-            ],
-            RFC::Web::Link::Models::Link[
-              uri: "https://test.io/articles?page=3",
-              pairs: Set[RFC::Web::Link::Models::Pair[key: :rel, value: "next"]]
-            ]
-          ]
-        ]
-      )
-    end
-
-    it "overrides root URI when supplied" do
-      headers = {"link" => "</articles>; rel=index"}
-
-      expect(link.call(headers, root_uri: "https://alt.io")).to eq(
-        RFC::Web::Link::Models::List[
-          links: Set[
-            RFC::Web::Link::Models::Link[
-              uri: "https://alt.io/articles",
-              pairs: Set[RFC::Web::Link::Models::Pair[key: :rel, value: "index"]]
-            ]
-          ]
-        ]
-      )
-    end
-
-    it "answers empty array when header key isn't found" do
-      expect(link.call({})).to eq(RFC::Web::Link::Models::List.new)
-    end
+    it_behaves_like "a header parser"
   end
 end
